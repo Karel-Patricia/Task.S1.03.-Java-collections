05997ca906b1608add91ab735453f3805c9e05a2
Patricia Paredes <patriciakarel@hotmail.com>
2026-01-10 13:30:31 +0100

    feat: añadir clase Month

---
+++ Nivel1/Ejercicio1/Ejercicio1N1/src/model/Month.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.Objects;
+
+public class Month {
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Month month = (Month) o;
+        return Objects.equals(name, month.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Month{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
